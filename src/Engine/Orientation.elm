0a2374e709b370405003df51e937cb4cdd7eaa4b
Isak <isakulstrup@gmail.com>
2024-09-25 11:07:34 +0200

    completed basic API

--- src/Engine/Orientation.elm
+++ src/Engine/Orientation.elm
@@ -1,29 +1,13 @@
-module Engine.Orientation exposing (..)
-
-
-type Radian
-    = Radian Float
-
-
-newAngle : Float -> Radian
-newAngle angle =
-    let
-        clamp x =
-            if x > 360 then
-                clamp (x - 360)
-
-            else if x < 0 then
-                clamp (360 - x)
-
-            else
-                x
-    in
-    Radian (angle |> clamp |> degrees)
-
-
-toDegrees : Radian -> Float
-toDegrees (Radian radian) =
-    radian * 180 / pi
+module Engine.Orientation exposing
+    ( Orientation
+    , new
+    , pitch
+    , pitchAngle
+    , roll
+    , rollAngle
+    , yaw
+    , yawAngle
+    )
 
 
 type alias Orientation =
@@ -36,3 +20,86 @@ type alias Orientation =
 new : Orientation
 new =
     Orientation (newAngle 0) (newAngle 0) (newAngle 0)
+
+
+pitch : Float -> Orientation -> Orientation
+pitch angle orientation =
+    { orientation | pitch = addAngle (newAngle angle) orientation.pitch }
+
+
+yaw : Float -> Orientation -> Orientation
+yaw angle orientation =
+    { orientation | yaw = addAngle (newAngle angle) orientation.yaw }
+
+
+roll : Float -> Orientation -> Orientation
+roll angle orientation =
+    { orientation | roll = addAngle (newAngle angle) orientation.roll }
+
+
+pitchAngle : Orientation -> Float
+pitchAngle orientation =
+    toDegrees orientation.pitch
+
+
+yawAngle : Orientation -> Float
+yawAngle orientation =
+    toDegrees orientation.yaw
+
+
+rollAngle : Orientation -> Float
+rollAngle orientation =
+    toDegrees orientation.roll
+
+
+
+-- RADIAN
+
+
+type Radian
+    = Radian Float
+
+
+{-| New radian from angle in degrees
+-}
+newAngle : Float -> Radian
+newAngle angle =
+    let
+        clamp x =
+            if x > 360 then
+                clamp (x - 360)
+
+            else if x < 0 then
+                clamp (360 + x)
+
+            else
+                x
+    in
+    Radian (clamp angle / 180)
+
+
+addAngle : Radian -> Radian -> Radian
+addAngle (Radian r1) (Radian r2) =
+    let
+        clamp x =
+            if x > 2 then
+                clamp (x - 2)
+
+            else if x < 0 then
+                clamp (2 - x)
+
+            else
+                x
+    in
+    Radian (r1 + r2 |> clamp)
+
+
+
+-- from degrees: a / 180
+-- to degrees: r * 180
+-- to rad: r * pi
+
+
+toDegrees : Radian -> Float
+toDegrees (Radian radian) =
+    radian * 180
